41399fe8c1a040b12a18914aad47f115a540c27c
Kazuki Sawada <kazuki@6715.jp>
2020-10-27 20:06:39 +0900

    impl: BoxProcessor

--- src/fs/box.ts
+++ src/fs/box.ts
@@ -2,6 +2,10 @@ import { AsyncProcessor, Tree, TreeNode } from ".";
 
 type BoxTreeNode = TreeNode & { id: string; };
 
+// type-definitions for `box-node-sdk` are currently not provided.
+// see https://github.com/box/box-node-sdk/issues/170
+type BoxClinet = any;
+
 type Entry = {
 	type: "file" | "folder" | "web_link";
 	id: string;
@@ -11,10 +15,10 @@ type Entry = {
 };
 
 export default class implements Tree<BoxTreeNode> {
-	private client: any;
+	private client: BoxClinet;
 	private rootId: string;
 
-	constructor(client: any, rootId: string) {
+	constructor(client: BoxClinet, rootId: string) {
 		this.client = client;
 		this.rootId = rootId;
 	}
@@ -63,6 +67,46 @@ export default class implements Tree<BoxTreeNode> {
 		return this.readFolder(await this.client.folders.get(this.rootId));
 	}
 	processor(): AsyncProcessor<BoxTreeNode, TreeNode> {
-		throw new Error("Method not implemented.");
+		return new BoxProcessor(this.client);
+	}
+}
+
+class BoxProcessor extends AsyncProcessor<BoxTreeNode, TreeNode> {
+	private client: BoxClinet;
+
+	constructor(client: BoxClinet) {
+		super();
+		this.client = client;
+	}
+
+	async create(parent: BoxTreeNode, node: TreeNode): Promise<void> {
+		console.log("box.create >>", node.name);
+
+		if (node.type == "file") {
+			await this.client.files.uploadFile(parent.id, node.name, await node.content());
+			return;
+		}
+		if (node.type == "folder") {
+			const ent: Entry = await this.client.folders.create(parent.id, node.name);
+			const created: BoxTreeNode = {
+				id: ent.id,
+				type: "folder",
+				name: ent.name,
+				entries: [],
+			};
+
+			await Promise.all(node.entries.map(ent => this.create(created, ent)));
+			return;
+		}
+	}
+	async delete(node: BoxTreeNode): Promise<void> {
+		console.log("box.delete >>", node.name);
+
+		if (node.type == "file") {
+			return this.client.files.delete(node.id);
+		}
+		if (node.type == "folder") {
+			return this.client.folders.delete(node.id, { recursive: true });
+		}
 	}
 }
